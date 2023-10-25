5b5bac07f91b6dd286c1d7324773ebd94937e49e
figogove <102362899+figogove@users.noreply.github.com>
2023-10-25 17:08:39 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,57 @@
 #!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+blue='\033[0;34m'
+ungu='\033[0;35m'
+Green="\033[32m"
+Red="\033[31m"
+WhiteB="\e[5;37m"
+BlueCyan="\e[5;36m"
+Green_background="\033[42;37m"
+Red_background="\033[41;37m"
+Suffix="\033[0m"
+#link izin ip vps
+url_izin='https://raw.githubusercontent.com/figogove/ip/master/ip'
+
+#IP VPS
+ip_vps=$(curl -sS ifconfig.me)
+
+# Mendapatkan isi file izin.txt dari URL
+izin=$(curl -s "$url_izin")
+
+# Memeriksa apakah konten izin.txt berhasil didapatkan
+if [[ -n "$izin" ]]; then
+  while IFS= read -r line; do
+    # Memisahkan nama VPS, IP VPS, dan tanggal kadaluwarsa
+    nama=$(echo "$line" | awk '{print $1}')
+    ipvps=$(echo "$line" | awk '{print $2}')
+    tanggal=$(echo "$line" | awk '{print $3}')
+
+    # Memeriksa apakah IP VPS saat ini cocok dengan IP VPS yang ada di izin.txt
+    if [[ "$ipvps" == "$ip_vps" ]]; then
+      echo "Nama VPS: $nama"
+      echo "IP VPS: $ipvps"
+      echo "Tanggal Kadaluwarsa: $tanggal"
+      break
+    fi
+  done <<< "$izin"
+
+  # Memeriksa apakah IP VPS ditemukan dalam izin.txt
+  if [[ "$ipvps" != "$ip_vps" ]]; then
+    echo -e ""
+    echo -e "${BlueCyan} ————————————————————————————————————————"
+    echo -e "      ${ungu}Terimakasih sudah menggunakan-"
+    echo -e "       Script Credit by RVPN STORES"
+    echo -e " ${BlueCyan}————————————————————————————————————————${Suffix}"
+    echo -e ""
+    exit 0
+  fi
+else
+  echo "Konten izin.txt tidak berhasil didapatkan dari URL"
+  exit 0
+fi
 ### Color
 Green="\e[92;1m"
 RED="\033[31m"
