fe363d5e17cfa8f8d1c42f7b8aeaac1395ab7b06
figogove <102362899+figogove@users.noreply.github.com>
2023-10-25 17:52:55 +0700

    Add files via upload

---
+++ badvpn/setup.sh
@@ -0,0 +1,20 @@
+ # // Installing UDP Mini
+REPO='https://raw.githubusercontent.com/figogove/vip/main/'
+mkdir -p /usr/local/kyt/
+wget -q -O /usr/local/kyt/badvpn "${REPO}badvpn/udp-mini"
+chmod +x /usr/local/kyt/badvpn
+wget -q -O /etc/systemd/system/badvpn1.service "${REPO}badvpn/udp-mini-1.service"
+wget -q -O /etc/systemd/system/badvpn2.service "${REPO}badvpn/udp-mini-2.service"
+wget -q -O /etc/systemd/system/badvpn3.service "${REPO}badvpn/udp-mini-3.service"
+systemctl disable badvpn1
+systemctl stop badvpn1
+systemctl enable badvpn1
+systemctl start badvpn1
+systemctl disable badvpn2
+systemctl stop badvpn2
+systemctl enable badvpn2
+systemctl start badvpn2
+systemctl disable badvpn3
+systemctl stop badvpn3
+systemctl enable badvpn3
+systemctl start badvpn3
