2d2db4dae7519f0d0bea4bf9d57992c9eaf49342
figogove <102362899+figogove@users.noreply.github.com>
2023-10-28 18:47:01 +0700

    Update setup.sh

--- badvpn/setup.sh
+++ badvpn/setup.sh
@@ -1,19 +1,19 @@
  # // Installing UDP Mini
 mkdir -p /usr/local/kyt/
-wget -q -O /usr/local/kyt/badvpn "https://raw.githubusercontent.com/figogove/vip/main/badvpn/udp-mini"
-chmod +x /usr/local/kyt/badvpn
-wget -q -O /etc/systemd/system/badvpn1.service "https://raw.githubusercontent.com/figogove/vip/main/badvpn/udp-mini-1.service"
-wget -q -O /etc/systemd/system/badvpn2.service "https://raw.githubusercontent.com/figogove/vip/main/badvpn/udp-mini-2.service"
-wget -q -O /etc/systemd/system/badvpn3.service "https://raw.githubusercontent.com/figogove/vip/main/badvpn/udp-mini-3.service"
-systemctl disable badvpn1
-systemctl stop badvpn1
-systemctl enable badvpn1
-systemctl start badvpn1
-systemctl disable badvpn2
-systemctl stop badvpn2
-systemctl enable badvpn2
-systemctl start badvpn2
-systemctl disable badvpn3
-systemctl stop badvpn3
-systemctl enable badvpn3
-systemctl start badvpn3
+wget -q -O /usr/local/kyt/udp-mini "https://raw.githubusercontent.com/figogove/vip/main/badvpn/udp-mini"
+chmod +x /usr/local/kyt/udp-mini
+wget -q -O /etc/systemd/system/udp-mini-1.service "https://raw.githubusercontent.com/figogove/vip/main/badvpn/udp-mini-1.service"
+wget -q -O /etc/systemd/system/udp-mini-2.service "https://raw.githubusercontent.com/figogove/vip/main/badvpn/udp-mini-2.service"
+wget -q -O /etc/systemd/system/udp-mini-3.service "https://raw.githubusercontent.com/figogove/vip/main/badvpn/udp-mini-3.service"
+systemctl disable udp-mini-1
+systemctl stop udp-mini-1
+systemctl enable udp-mini-1
+systemctl start udp-mini-1
+systemctl disable udp-mini-2
+systemctl stop udp-mini-2
+systemctl enable udp-mini-2
+systemctl start udp-mini-2
+systemctl disable udp-mini-3
+systemctl stop udp-mini-3
+systemctl enable udp-mini-3
+systemctl start udp-mini-3
